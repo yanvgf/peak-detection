18da516d4d729b90fce80f7092adb313ad7a1cea
yanvgf <engyanvictor@gmail.com>
2023-09-23 18:07:38 -0300

    Optimize S1

--- utils.py
+++ utils.py
@@ -13,16 +13,18 @@ def s1_metric(data, k):
         s1 (pd.DataFrame): series with the values of the S1 metric for each point
     """
     
+    data = data.to_numpy()
+    
     s1 = np.zeros(data.shape[0])
     
     # For each sample in data, calculates the difference between its value and its k neighbors to the right and left
     for idx in range(data.shape[0]):
-        max_right = np.max([data.iloc[idx,:].values[0] - data.iloc[idx+k_i+1,:].values[0]
+        max_right = np.max([(data[idx] - data[idx+k_i+1])[0]
                         if idx+k_i+1 < data.shape[0] else 0
-                        for k_i in range(k)])
-        max_left = np.max([data.iloc[idx,:].values[0] - data.iloc[idx-(k_i+1),:].values[0] 
+                        for k_i in range(k)], axis=0)
+        max_left = np.max([(data[idx] - data[idx-(k_i+1)] )[0]
                         if idx-(k_i+1) >= 0 else 0
-                        for k_i in range(k)])
+                        for k_i in range(k)], axis=0)
         s1[idx] = (max_right + max_left)/2
         
     return pd.DataFrame(s1)
